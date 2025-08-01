1cd8482532e17eaf9365dafb88c2d305a8e10f52
robert thornton <robert.thornton92@gmail.com>
2025-08-01 21:28:58 +0300

    Updated Node package modules

--- database/query.js
+++ database/query.js
@@ -3,6 +3,18 @@ const bcrypt = require('bcrypt');
 
 const db = {
 
+    async signup({ username, firstName, lastName, email, hashedPassword }) {
+        try {
+            await pool.query(
+                'INSERT INTO users (username, first, last, email, password) VALUES ($1, $2, $3, $4, $5) RETURNING id',
+                [username, firstName, lastName, email, hashedPassword]
+            );
+            
+        } catch(err) {
+            
+            throw new Error('Database error during signup:' + err);
+        }
+    }
 }
 
 module.exports = db;
