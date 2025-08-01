f804b5daf9bec2249157efbc2f8b94967b1b2439
robert thornton <robert.thornton92@gmail.com>
2025-08-01 20:55:12 +0300

    Added: router and authorization boiler plate code for passport.js

--- app.js
+++ app.js
@@ -5,41 +5,45 @@ const path = require('path');
 const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: false }));
 
 // Database connection
 const pool = require('./database/pool');
 
 // Routers
+const userRouter = require('./routers/userRouter');
+app.use('/user', userRouter);
 
 // Auth and Sessions
 const session = require('express-session');
+const pgSession = require('connect-pg-simple')(session);
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
 
 app.use(session({
+    store: new pgSession({
+        pool: pool,
+        tableName: 'session'
+    }),
     secret: process.env.SESSION_SECRET,
     resave: false,
     saveUninitialized: false,
+    cookie: { maxAge: 1000 * 60 * 60 * 24 }
     })
 );
+app.use(passport.initialize());
 app.use(passport.session());
 
-
-app.use(express.urlencoded({ extended: true }));
-
+app.use((req, res, next) => {
+    console.log(req.session);
+    console.log(req.user);
+    next();
+})
 
 app.get('/', (req, res) => {
     res.render('index', { user: req.user });
 });
 
-app.get('/sign-up', (req, res) => {
-    res.render('signup')
-})
-
-app.post('/sign-up', async (req,res) => {
-    const { username, first, last, email, password } = req.body;
-    res.render('index');
-    })
 
 app.listen(process.env.PORT, () => {
     console.log('server running on port ' + process.env.PORT);
