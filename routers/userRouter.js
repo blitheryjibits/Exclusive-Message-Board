d398de99fa45d7c612784ac2d53217fc3b2bd44e
robert thornton <robert.thornton92@gmail.com>
2025-08-21 14:04:01 +0300

    Feat: implemented routes and passport authentication

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -1,6 +1,7 @@
 const { Router } = require('express');
 const userRouter = Router();
 const controller = require('../controllers/controller');
+const passport = require('passport');
 
 userRouter.get('/signup', controller.signupForm);
 
@@ -8,6 +9,19 @@ userRouter.post('/signup', controller.signup);
 
 userRouter.get('/signin', controller.signinForm);
 
-userRouter.post('/signin', controller.signin);
+userRouter.post('/signin', passport.authenticate('local', {
+    successRedirect: '/',
+    failureRedirect: '/user/signin'
+    // failureFlash: true
+}));
+
+userRouter.get('/logout', controller.logout);
+
+userRouter.get('/create_message', controller.createMessage);
+
+userRouter.post('/create_message', controller.postMessage);
+
+userRouter.get('/join', (req, res) => {res.render('member-status')});
+userRouter.post('/join', controller.addMember);
 
 module.exports = userRouter;
