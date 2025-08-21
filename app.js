39397a031093b6204ac94f2a45b0ad732701d674
robert thornton <robert.thornton92@gmail.com>
2025-08-21 14:00:39 +0300

    Refactor - removed redundant code and added json parser

--- app.js
+++ app.js
@@ -6,19 +6,22 @@ const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(express.static('public'));
+
 
 // Database connection
 const pool = require('./database/pool');
 
 // Routers
 const userRouter = require('./routers/userRouter');
-app.use('/user', userRouter);
 
 // Auth and Sessions
 const session = require('express-session');
 const pgSession = require('connect-pg-simple')(session);
 const passport = require('passport');
-const LocalStrategy = require('passport-local').Strategy;
+const controller = require('./controllers/controller');
+require('./config/passport');
 
 app.use(session({
     store: new pgSession({
@@ -35,15 +38,19 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 app.use((req, res, next) => {
-    console.log(req.session);
-    console.log(req.user);
+    res.locals.user = req.user;
     next();
 })
 
-app.get('/', (req, res) => {
-    res.render('index', { user: req.user });
+// Routes 
+
+app.get('/', async (req, res) => {
+    const messages = await controller.retrieveMessages(req, res);
+    console.log(messages);
+    res.render('index', {messages: messages});
 });
 
+app.use('/user', userRouter);
 
 app.listen(process.env.PORT, () => {
     console.log('server running on port ' + process.env.PORT);
